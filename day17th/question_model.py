d6d13a9ad961dc9b48a4115ed7a72ff0935df41e
wherethacoffee <whr.thacoff23@gmail.com>
2023-02-24 17:46:41 -0600

    Question program for the quiz project added

---
+++ day17th/question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+new_q = Question("2+3=5", "True")
+
+print(new_q.text, new_q.answer)
