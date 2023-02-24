6690ffc323823603f5dd18982a6a80983155752d
wherethacoffee <whr.thacoff23@gmail.com>
2023-02-24 17:42:45 -0600

    Class practice program finished

--- day17th/classTest.py
+++ day17th/classTest.py
@@ -1,4 +1,20 @@
 class User:
-    pass
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
 
-user_1 = User()
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+user_1 = User("001", "Jorge")
+user_2 = User("002", "Vanessa")
+
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
