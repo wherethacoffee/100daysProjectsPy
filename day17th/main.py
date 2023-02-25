735a264e07385944902c98ec1def9e5fe5a782bd
wherethacoffee <whr.thacoff23@gmail.com>
2023-02-24 20:21:38 -0600

    Main file program updated

--- day17th/main.py
+++ day17th/main.py
@@ -1,2 +1,14 @@
 from question_model import Question
 from data import question_data
+
+question_bank = []
+
+for item in question_data:
+    q = item['text']
+    a = item['answer']
+
+    new_question = Question(q, a)
+    question_bank.append(new_question)
+
+
+
