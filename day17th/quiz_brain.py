12c5bce7db575323a1166adab4f0f95a4f8261f9
wherethacoffee <whr.thacoff23@gmail.com>
2023-02-24 21:54:50 -0600

    Project done

---
+++ day17th/quiz_brain.py
@@ -0,0 +1,25 @@
+class QuizBrain:
+    def __init__(self, q_list) -> None:
+        self.question_number = 0
+        self.questions_list = q_list
+        self.score = 0
+
+    def next_question(self):
+        current_question = self.questions_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)? ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def still_have_questions(self):
+        return  self.question_number < len(self.questions_list)
+    
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("You got it right!")
+        else:
+            print("That's wrong")
+        print(f"The correct answer is: {correct_answer}")
+        print(f"Your current score is: ({self.score}/{self.question_number})")
+        print("\n")
+        
