38066a218bfb8d69e3e4bf6f9764713bfac31933
wherethacoffee <whr.thacoff23@gmail.com>
2023-02-24 21:54:12 -0600

    Data file updated

--- day17th/data.py
+++ day17th/data.py
@@ -1,14 +1,18 @@
 question_data = [
-{"text": "A slug's blood is green.", "answer": "True"},
-{"text": "The loudest animal is the African Elephant.", "answer": "False"},
-{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-{"text": "Google was originally called 'Backrub'.", "answer": "True"},
-{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+  {"category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"The Indie Game Development Studio Cing, developers of Hotel Dusk and Last Window, went bankrupt on March 1st, 2010.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"General Knowledge","type":"boolean","difficulty":"hard","question":"This is the correct spelling of 'Supercalifragilisticexpialidocious';.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Politics","type":"boolean","difficulty":"easy","question":"The S in Harry S. Truman stands for 'Samuel';.","correct_answer":"False","incorrect_answers":["True"]},
+  {"category":"Geography","type":"boolean","difficulty":"medium","question":"Vietnam is the only country in the world that starts with V. ","correct_answer":"False","incorrect_answers":["True"]},
+  {"category":"Entertainment: Board Games","type":"boolean","difficulty":"easy","question":"The card game 'Munchkin'; won the 2001 Origins Award for Best Traditional Card Game.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"General Knowledge","type":"boolean","difficulty":"easy","question":"On average, at least 1 person is killed by a drunk driver in the United States every hour.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"History","type":"boolean","difficulty":"medium","question":"In 1967, a magazine published a story about extracting hallucinogenic chemicals from bananas to raise moral questions about banning drugs.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Entertainment: Music","type":"boolean","difficulty":"easy","question":"The song 'Stronger Than You'; is a single by Estelle, who played Garnet in Steven Universe.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Geography","type":"boolean","difficulty":"easy","question":"California is larger than Japan.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"Valve's 'Portal'; and 'Half-Life'; franchises exist within the same in-game universe.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Entertainment: Television","type":"boolean","difficulty":"medium","question":"An episode of 'The Simpsons'; is dedicated to Moe Szyslak's bar rag.","correct_answer":"True","incorrect_answers":["False"]},
+  {"category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"'Sonic the Hedgehog 2'; originally was going to have a time travel system.","correct_answer":"True","incorrect_answers":["False"]},
+  {"question":"Is Bobby an alucin", "correct_answer":"True"},
+  {"question":"Are you dumb", "correct_answer":"True"},
+  {"question":"Is Jorge a bad person", "correct_answer":"False"}
+
 ]
