63d7909b97aa8af9925c5a75aec6246e221cecc3
wherethacoffee <whr.thacoff23@gmail.com>
2023-03-16 21:48:04 -0600

    Food class completed

--- day20th/snake_game/food.py
+++ day20th/snake_game/food.py
@@ -1,4 +1,15 @@
-class Food:
+from turtle import Turtle
+import random
 
-    def __init__(self) -> None:
-        pass
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("yellow")
+        self.speed("fastest")
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
